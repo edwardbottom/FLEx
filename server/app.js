4282f7b94b49dfd8021df808918f2f56b31af6e3
Edward Bottom <edward.bottom@wustl.edu>
2018-09-21 09:39:19 -0500

    stack connections

--- server/app.js
+++ server/app.js
@@ -1,41 +1,53 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express')
+var cors = require('cors')
+const app = express()
+const port = 3000
+var path = require("path");
+var bodyParser = require("body-parser");
+var mongodb = require("mongodb");
+var ObjectID = mongodb.ObjectID;
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+//set up cors requests
+app.use(cors())
 
-var app = express();
+//mongo credentials
+const MongoClient = require('mongodb').MongoClient;
+const MONGO_URL = 'mongodb://Ed:drowssap1@ds058739.mlab.com:58739/flex';
+var db;
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+//connect to database and run app
+mongodb.MongoClient.connect(MONGO_URL, function (err, database) {
+  if (err) {
+    console.log(err);
+    process.exit(1);
+  }
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+  // Save database object from the callback for reuse.
+  db = database.db('flex');
+  console.log("Database connection ready");
+  app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+})
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+//hello world get request
+app.get('/', (req, res) => res.send('Hello World!'))
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
+//places a single json object in the database
+app.get('/db', (req, res) =>{
+	//insert a user into the databse
+  	db.collection('Users').insertOne(
+ 	{
+      	user:'name'
+    },
+    //catch errors
+    function (err, res) {
+      	if (err) {
+        	db.close();
+        	res.send("it done fucked up")
+      	}
+      	// Success
+      	db.close();  
+    });
+	res.send("this path works")
+})
--- ui/app/src/main.js
+++ ui/app/src/main.js
@@ -4,5 +4,18 @@ import App from './App.vue'
 Vue.config.productionTip = false
 
 new Vue({
+  created: function(){
+  	const axios = require('axios');
+	   //Make a request for a user with a given ID
+	   axios.get('http://localhost:3000/')
+  	.then(function (response) {
+    	// handle success
+    	console.log(response);
+  	})
+  	.catch(function (error) {
+    	// handle error
+    	console.log(error);
+  	})
+  },
   render: h => h(App)
 }).$mount('#app')
