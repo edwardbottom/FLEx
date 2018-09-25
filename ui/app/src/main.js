3e2e57cb4b870482b253d94a3d8af49d4bb5c21a
Edward Bottom <edward.bottom@wustl.edu>
2018-09-24 20:41:07 -0500

    updates

--- ui/app/src/main.js
+++ ui/app/src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+// import router from './router'
+
 
 Vue.config.productionTip = false
+
+
 
 new Vue({
   created: function(){
@@ -19,3 +23,10 @@ new Vue({
   },
   render: h => h(App)
 }).$mount('#app')
+
+// new Vue({
+//   el: '#app',
+//   router,
+//   template: '<App/>',
+//   components: { App }
+// })
