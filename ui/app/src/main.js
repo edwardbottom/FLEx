543f77e48ef35871a821120e3d4c424ba134cf83
Edward Bottom <edward.bottom@wustl.edu>
2018-09-25 17:45:48 -0500

    bootstrap updates

--- ui/app/src/main.js
+++ ui/app/src/main.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router/index'
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
+
+Vue.use(BootstrapVue);
 // Enable devTools
 if (window.location.hostname.indexOf('local') > -1) {
   Vue.config.devtools = true
