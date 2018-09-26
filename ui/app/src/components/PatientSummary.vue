5e6b0e5d0e689c6598bd84da1ccab60c283c2682
Edward Bottom <edward.bottom@wustl.edu>
2018-09-25 19:05:43 -0500

    something

---
+++ ui/app/src/components/PatientSummary.vue
@@ -0,0 +1,37 @@
+<template>
+  <div v-for="patient in patients" class="PatientSummary">
+  <b-btn v-b-toggle.collapse1 variant="primary">Toggle Collapse</b-btn>
+    <b-collapse id="collapse1" class="mt-2">
+      <b-card>
+        <p class="card-text">Collapse contents Here</p>
+      </b-card>
+  </b-collapse>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PatientSummary',
+  props: {
+    patients:[
+      p1:{
+        name:"steve",
+        age:37,
+        height:"6'0",
+        weight: 190,
+        status:'bad',
+        firstVisit:"1/1/2017",
+        lastVist:"1/1/2018",
+        injury:"broken arm",
+        currentStatus:"3/10",
+        averageStatus:"5/10",
+        lastUsedApp:"4/4/2017",
+        insuranceProvider:"United HealthCare",
+        contact:"314-666-6666",
+        nextVist:"n/a"
+      }
+    ]
+  }
+}
+</script>
+
