45d9ad4e476411afda63d6ffd00276301e7e8784
JTBoschelli <35546197+JTBoschelli@users.noreply.github.com>
2018-09-25 21:23:41 -0500

    main

--- ui/app/src/main.js
+++ ui/app/src/main.js
@@ -4,9 +4,11 @@ import router from './router/index'
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import DatePicker from 'vuejs-datepicker'
 
 
 Vue.use(BootstrapVue);
+Vue.use(DatePicker);
 // Enable devTools
 if (window.location.hostname.indexOf('local') > -1) {
   Vue.config.devtools = true
