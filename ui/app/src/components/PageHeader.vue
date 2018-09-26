86e55019f6f16f0adf58cfd36597e71d9364f4d9
JTBoschelli <35546197+JTBoschelli@users.noreply.github.com>
2018-09-25 21:20:28 -0500

    Pageheader

---
+++ ui/app/src/components/PageHeader.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="topBar">
+    <b-navbar toggleable="md" type="dark" variant="danger">
+
+      <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+
+      <b-navbar-brand href="#">FLEx</b-navbar-brand>
+
+      <datepicker placeholder="Select Date"></datepicker>
+
+      <b-collapse is-nav id="nav_collapse">
+
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+      
+          
+
+          <b-nav-item-dropdown right>
+            <!-- Using button-content slot -->
+            <template slot="button-content">
+              <em>User</em>
+            </template>
+            <b-dropdown-item href="#">Profile</b-dropdown-item>
+            <b-dropdown-item href="#">Signout</b-dropdown-item>
+          </b-nav-item-dropdown>
+        </b-navbar-nav>
+
+      </b-collapse>
+    </b-navbar>
+
+    
+
+  </div>
+
+
+</template>
+
+<script>
+  import Datepicker from "vuejs-datepicker/dist/vuejs-datepicker.esm.js";
+export default {
+  name: 'PageHeader',
+  props: {
+    msg: String
+  },
+  components: {
+    Datepicker
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
