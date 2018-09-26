4673dda3a4358c7f5baf239e2bdd49aab093188a
JTBoschelli <35546197+JTBoschelli@users.noreply.github.com>
2018-09-25 21:21:07 -0500

    Userhome

--- ui/app/src/components/UserHome.vue
+++ ui/app/src/components/UserHome.vue
@@ -1,36 +1,18 @@
 <template>
   <div class="topBar">
-    <b-navbar toggleable="md" type="dark" variant="info">
-
-  <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
-
-  <b-navbar-brand href="#">FLEx</b-navbar-brand>
-
-  <b-collapse is-nav id="nav_collapse">
-
-    <!-- Right aligned nav items -->
-    <b-navbar-nav class="ml-auto">
-
-      <b-nav-item-dropdown right>
-        <!-- Using button-content slot -->
-        <template slot="button-content">
-          <em>User</em>
-        </template>
-        <b-dropdown-item href="#">Profile</b-dropdown-item>
-        <b-dropdown-item href="#">Signout</b-dropdown-item>
-      </b-nav-item-dropdown>
-    </b-navbar-nav>
-
-  </b-collapse>
-</b-navbar>
+    <PageHeader></PageHeader>
   </div>
 </template>
 
 <script>
+  import PageHeader from "./PageHeader.vue";
 export default {
   name: 'UserHome',
   props: {
     msg: String
+  },
+  components: {
+    PageHeader
   }
 }
 </script>
